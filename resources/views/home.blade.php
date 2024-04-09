48b0aaaad0bbdd67e036cade0bdc5bc789f528fa
federico_viganò <fede_viga@hotmail.it>
2024-04-09 12:02:06 +0200

    create cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,38 @@
 
 <main class="container text-white py-5">
 
-    @foreach($movies as $movie)
-        <li>
-            {{$movie->title}}
-        </li>
-    @endforeach
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+
+        @foreach($movies as $movie)
+            <div class="col p-2">
+
+                <div class="card h-100 bg-black text-danger text-center p-3">
+
+                    <h2>
+                        {{$movie->title}}
+                    </h2>
+
+                    @if("{{$movie->title}}" != "{{$movie->original_title}}")
+                        <h3>
+                            Original Title: {{$movie->original_title}}
+                        </h3>
+                    @endif
+
+                    <h4>
+                        Nationality: {{$movie->nationality}}
+                    </h4>
+
+                    <h4>
+                        Release: {{$movie->date}}
+                    </h4>
+
+                    <h4>
+                        Average vote: {{$movie->vote}}
+                    </h4>
+                </div>
+            </div>
+        @endforeach
+        </div>
 
 </main>
 
